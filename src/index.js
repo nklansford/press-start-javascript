4b28221c8fa56748cff711302ca8eb5c7820df9c
Noelle Lansford <noelle@heysparkbox.com>
2022-07-06 22:50:32 -0400

    feat: init commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Toggle Animations
+// const controlPlayPause = document.getElementById("control-play-pause");
+// controlPlayPause.addEventListener("click", () => {
+//   const animations = document.querySelector("data-animation");
+//   animations.forEach((animation) => {
+//     const running = animation.style.animationPlayState || "running";
+//     animation.style.animationPlayState =
+//       running === "running" ? "paused" : "running";
+//   });
+// });
+
+console.log("hey gurl");
